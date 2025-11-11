a2f35fccdb9a63ca6ceb72ce8e7701a303a78dc4
Babatunde Awoyemi <tunspiderman@gmail.com>
2025-11-11 16:39:52 +0100

    update

---
+++ submissions/David_Adetola/David_Adetola_Quizgame/quiz game.py
@@ -0,0 +1,9 @@
+input("correct")
+'else''wrong'
+input("wrong")
+'else''correct'
+print("i am something i am the beginning of the end and the end of time what am i")
+print("i am a brother that can never meet his sisters what am i")
+print("2x+5x-5=0 solve for x")
+print("5+4+3+2+1x0")
+print("solve the polynomial:p1(x)=3x^2-4x+34 and p2(x)=4x^3+3x^2-2x+3 find p1(x)+p2(x)")
---
+++ submissions/David_Adetola/David_Adetola_StudentGradeAnalyser/student grade analyser.py
@@ -0,0 +1,26 @@
+input("name of student a?")
+input("grade of student a?")
+"if"'grade passes 70'
+print("a")
+'else''b'
+input("name of student b?")
+input("grade of student b?")
+"if"'grade passes 70'
+print("a")
+'else''b'
+input("name of student c?")
+input("grade of student c?")
+"if"'grade passes 70'
+print("a")
+'else''b'
+input("name of student d?")
+input("grade of student d?")
+"if"'grade passes 70'
+print("a")
+'else''b'
+input("name of student e?")
+input("grade of student e?")
+"if"'grade passes 70'
+print("a")
+'else''b'
+
